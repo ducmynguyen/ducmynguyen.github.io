a10c7ec1d9a84c2955173c16aeff9429da4e0610
ducmynguyen <nguyenducmy393@gmail.com>
2023-04-09 18:43:29 +0700

    update section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,57 @@
-console.log("Hello World!");
+$(document).ready(function() {
+    //Menu button on click event
+    $('.mobile-nav-button').on('click', function() {
+        // Toggles a class on the menu button to transform the burger menu into a cross
+        $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(1)").toggleClass("mobile-nav-button__line--1");
+        $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(2)").toggleClass("mobile-nav-button__line--2");
+        $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(3)").toggleClass("mobile-nav-button__line--3");
+
+        // Toggles a class that slides the menu into view on the screen
+        $('.mobile-menu').toggleClass('mobile-menu--open');
+        return false;
+    });
+});
+
+$(document).ready(function() {
+    $('.navigation__link').bind('click', function(e) {
+        e.preventDefault(); // prevent hard jump, the default behavior
+
+        var target = $(this).attr("href"); // Set the target as variable
+
+
+        // perform animated scrolling by getting top-position of target-element and set it as scroll target
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 600, function() {
+            location.hash = target; //attach the hash (#jumptarget) to the pageurl
+        });
+
+        $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(1)").toggleClass("mobile-nav-button__line--1");
+        $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(2)").toggleClass("mobile-nav-button__line--2");
+        $(".mobile-nav-button .mobile-nav-button__line:nth-of-type(3)").toggleClass("mobile-nav-button__line--3");
+
+        // Toggles a class that slides the menu into view on the screen
+        $('.mobile-menu').toggleClass('mobile-menu--open');
+
+        return false;
+    });
+});
+
+$(window).scroll(function() {
+    var scrollDistance = $(window).scrollTop();
+
+    // Show/hide menu on scroll
+    //if (scrollDistance >= 850) {
+    //		$('nav').fadeIn("fast");
+    //} else {
+    //		$('nav').fadeOut("fast");
+    //}
+
+    // Assign active class to nav links while scolling
+    $('.page-section').each(function(i) {
+        if ($(this).position().top <= scrollDistance) {
+            $('.navigation a.active').removeClass('active');
+            $('.navigation a').eq(i).addClass('active');
+        }
+    });
+}).scroll();
