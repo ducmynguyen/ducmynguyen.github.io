e072442d02d69186562794ff452d779d3d99fb5c
ducmynguyen <nguyenducmy393@gmail.com>
2023-04-09 19:55:28 +0700

    add scroll to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,20 @@ $(window).scroll(function() {
         }
     });
 }).scroll();
+
+//Scroll to top
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
